ea341ff3ff77e69ac708696c0e7b2576cbe9b74a
CarlosRTZz <or.carlos.pro@gmail.com>
2024-02-26 19:32:01 +0100

    debug change

--- Makefile
+++ Makefile
@@ -5,15 +5,15 @@ conf:
 	mkdir -p ~/data/wordpress_data
 
 build: clean
-	sudo docker-compose -f srcs/docker-compose.yml up -d
+	docker-compose -f srcs/docker-compose.yml up -d
 # lance le container
 up: down
-	sudo docker-compose -f srcs/docker-compose.yml up
+	docker-compose -f srcs/docker-compose.yml up
 # arete le container
 down:
-	sudo docker-compose -f srcs/docker-compose.yml down
+	docker-compose -f srcs/docker-compose.yml down
 
 clean:
-	sudo docker-compose -f srcs/docker-compose.yml down --rmi all --volumes
+	docker-compose -f srcs/docker-compose.yml down --rmi all --volumes
 
 .PHONY: all build up down clean
--- srcs/requirements/wordpress/tools/wp_script.sh
+++ srcs/requirements/wordpress/tools/wp_script.sh
@@ -6,6 +6,14 @@ sleep 10
 # Listen to 9000
 sed -i "s|listen = /run/php/php7.4-fpm.sock|listen = 9000|" /etc/php/7.4/fpm/pool.d/www.conf
 
+echo "je cd dans wordpress"
+
+cd /var/www/wordpress
+
+echo $PWD
+
+echo "JE CONFIG"
+
 # Config Wordpress
 wp config create \
 		--dbname="${DB_NAME}" \
@@ -13,6 +21,9 @@ wp config create \
 		--dbpass="${DB_PASSWORD}" \
 		--dbhost="${DB_HOST}" \
         --allow-root
+
+echo "c'est config"
+echo "j'installe"
 
 # Install WordPress
 wp core install --url="${DOMAIN_NAME}" \
@@ -22,12 +33,17 @@ wp core install --url="${DOMAIN_NAME}" \
     --admin_email="${WP_ADMIN_EMAIL}" \
     --allow-root
 
+echo "c'est install"
+echo "j'insatlle un user wp"
+
 # Add new WordPress user
 wp user create "$WP_USER" "$WP_EMAIL" \
     --user_pass="${WP_PASSWORD}" \
     --role="${WP_ROLE}" \
     --allow-root
 
+echo "SA FONCTIONNE"
+
 # Start PHP FastCGI Process Manager
 # PHP-FPM is responsible for handling PHP requests.
-/usr/sbin/php-fpm7.4 -F
+/usr/sbin/php-fpm7.4 -F -R
